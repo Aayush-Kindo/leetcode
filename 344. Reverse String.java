e448593637be34b6bad62bca8f387a69124e520a
Aayush Kindo <aayush.kindo@gmail.com>
2022-04-11 23:52:14 +0530

    Create 344. Reverse String.java

---
+++ 344. Reverse String.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        reverse(s,0,s.length-1);
+    }
+    
+    static void reverse(char[]s,int i,int j){
+        if(i>j){
+            return;
+        }
+        char temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        
+        reverse(s,++i,--j);
+    }
+}
